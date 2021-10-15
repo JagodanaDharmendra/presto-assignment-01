a1db319512326196e18fdcc87da03ff0ae818a44
DharmendraKumar Jagodana <jagodana.dharmendra@gmail.com>
2021-10-15 17:27:12 +0530

    slider updated

--- src/components/homescreen/slider.jsx
+++ src/components/homescreen/slider.jsx
@@ -1,22 +1,17 @@
 import { Carousel } from "antd";
 
-const data = [
-  {
-    src: "./images/banner1.jpg",
-  },
-  {
-    src: "./images/banner2.jpg",
-  },
-];
-
-export default function Slider({}) {
+export default function Slider({ data }) {
   return (
     <div>
       <Carousel effect="slidex" autoplay>
         {data?.map((current) => (
           <div className="">
             <div className="w-screen max-h-111 h-111">
-              <img src={current.src} className="w-full object-cover h-full" />
+              <img
+                src={current.src}
+                alt={current.alt}
+                className="w-full object-cover h-full"
+              />
             </div>
           </div>
         ))}
