2edb11fd52669430473c37203d4d915f58ef11e7
DharmendraKumar Jagodana <47411494+JagodanaDharmendra@users.noreply.github.com>
2021-10-16 09:23:20 +0530

    added see all button and it's click event

--- src/components/horizontalScrollView.jsx
+++ src/components/horizontalScrollView.jsx
@@ -5,15 +5,25 @@ export default function HorizontalScrollView(props) {
     <div
       className={
         "py-3 " +
-        (props.background && props.background == true
+        (props.background && props.background === true
           ? "from-white to-gray-300 bg-gradient-to-b"
           : "")
       }
     >
       <div className="px-8">
-        <h2 className="font-semibold text-lg text-left align-middle">
-          {props.title}
-        </h2>
+        <div className="flex flew-row justify-between py-2">
+          <div className="font-semibold text-lg text-left align-middle">
+            {props.title}
+          </div>
+          <div
+            className=" cursor-pointer font-semibold text-lg text-left align-middle text-blue-600"
+            onClick={() => {
+              props.onClickSeeAll();
+            }}
+          >
+            See All
+          </div>
+        </div>
       </div>
       <div>
         <div className="flex overflow-x-scroll scrollbar-none no-scrollbar bg-transparent">
