6c4bdd8c3128f4b8e38a7961f8994b9090c865bc
DharmendraKumar Jagodana <jagodana.dharmendra@gmail.com>
2021-10-19 01:00:44 +0530

    added sentry logging and tracing

--- src/index.js
+++ src/index.js
@@ -12,6 +12,21 @@ import { Provider } from "react-redux";
 
 import store from "./redux/store";
 
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://563cb1dc190b4e88925315400f0b1196@o1043260.ingest.sentry.io/6012771",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+Sentry.captureMessage("Application Started");
+
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
